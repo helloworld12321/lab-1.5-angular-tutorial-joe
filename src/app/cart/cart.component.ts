709b69006e96a5f1e34696d640bc0534cbdeefb3
helloworld12321 <walbr037@morris.umn.edu>
2020-02-04 22:38:42 -0600

    Fix a bug where CartComponent wasn't updating after CartService.clearCart was called

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -7,14 +7,13 @@ import { CartService } from '../cart.service';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-  items;
-
   constructor(
     private cartService: CartService,
   ) { }
 
-  ngOnInit() {
-    this.items = this.cartService.getItems();
-  }
+  ngOnInit() { }
 
+  getItems() {
+    return this.cartService.getItems();
+  }
 }
